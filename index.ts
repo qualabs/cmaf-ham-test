56ff180addc93470d16f62affb08c6769a35a183
Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>
2024-03-25 11:56:50 -0300

    Refactor code

--- index.ts
+++ index.ts
@@ -11,18 +11,30 @@ import fs from 'fs';
 const DIR_MANIFEST_SAMPLES = 'manifest-samples';
 const DIR_DASH_SAMPLES = `${DIR_MANIFEST_SAMPLES}/dash-samples`;
 const DIR_HLS_SAMPLES = `${DIR_MANIFEST_SAMPLES}/hls-samples`;
+const HLS_EXTENSION = '.m3u8';
 
 function test (dirMainManifest:string, ancillaryManifestss?:string[]){
   const manifest = fs.readFileSync(dirMainManifest, 'utf8');
-  const ham = dashToHam(manifest);
+
+  //Convert the manifest to HAM
+  const ham = dirMainManifest.endsWith(HLS_EXTENSION) ? hlsToHam(manifest,ancillaryManifestss) : dashToHam(manifest);
   fs.writeFileSync('ham.json', JSON.stringify(ham));
+
+  //Convert the HAM to DASH
   const dash = hamToDash(ham);
   
-  const m3u8 = hamToHls(ham);
-  fs.writeFileSync('main.m3u8', m3u8.manifest);
+  //Write the manifest to a file
   fs.writeFileSync('main.mpd', dash.manifest);
+
+  // Convert the HAM to HLS
+  const hls = hamToHls(ham);
+
+  // Write the manifest to a file
+  fs.writeFileSync('main.m3u8', hls.manifest);
+
+  // Write the ancillary manifests to files
   
-  m3u8.ancillaryManifests?.forEach((ancillaryManifest: { manifest: string | NodeJS.ArrayBufferView; }, index: any) => {
+  hls.ancillaryManifests?.forEach((ancillaryManifest: { manifest: string | NodeJS.ArrayBufferView; }, index: any) => {
     fs.writeFileSync(`ancillary-${index}.m3u8`, ancillaryManifest.manifest);
   });
 }
