5e4a828b46077728e18be5bd4a457784f0fbee4f
Noelia Bentancor <71080743+NoeliaBentancor@users.noreply.github.com>
2024-03-26 15:48:04 -0300

    Delete inputs

--- constants.ts
+++ constants.ts
@@ -8,7 +8,7 @@ const FILE_ENCODING = 'utf8';
 
 //This is the manifest that will be used for testing. Change this to test different manifests.
 const MANIFEST_TEST = `${DIR_HLS_SAMPLES}/sample-1/main.m3u8`;
-const PLAYLISTS_TESTS = [
+const PLAYLISTS_TESTS_1 = [
     `${DIR_HLS_SAMPLES}/sample-1/playlist_a-eng-0128k-aac-2c.mp4.m3u8`,
     `${DIR_HLS_SAMPLES}/sample-1/playlist_v-0144p-0100k-libx264.mp4.m3u8`,
     `${DIR_HLS_SAMPLES}/sample-1/playlist_v-0240p-0400k-libx264.mp4.m3u8`,
@@ -16,6 +16,16 @@ const PLAYLISTS_TESTS = [
     `${DIR_HLS_SAMPLES}/sample-1/playlist_v-0480p-1000k-libx264.mp4.m3u8`,
     `${DIR_HLS_SAMPLES}/sample-1/playlist_v-0576p-1400k-libx264.mp4.m3u8`,
     `${DIR_HLS_SAMPLES}/sample-1/playlist_v-0720p-2500k-libx264.mp4.m3u8`,
+]
+
+const PLAYLISTS_TESTS = [
+    `${DIR_HLS_SAMPLES}/sample-3/audio-eng-038k-aac-6c.mp4.m3u8`,
+    `${DIR_HLS_SAMPLES}/sample-3/audio-eng-0128k-aac-2c.mp4.m3u8`,
+    `${DIR_HLS_SAMPLES}/sample-3/playlist_s-el.webvtt.m3u8`,
+    `${DIR_HLS_SAMPLES}/sample-3/playlist_s-en.webvtt.m3u8`,
+    `${DIR_HLS_SAMPLES}/sample-3/video-0360p-0750k-libx264.mp4.m3u8`,
+    `${DIR_HLS_SAMPLES}/sample-3/video-0480p-1000k-libx264.mp4.m3u8`,
+    `${DIR_HLS_SAMPLES}/sample-3/video-0576p-1400k-libx264.mp4.m3u8`,
 ]
 
 export { DIR_MANIFEST_SAMPLES, DIR_DASH_SAMPLES, DIR_HLS_SAMPLES, OUTPUT_DIR, HLS_EXTENSION, FILE_ENCODING, MANIFEST_TEST , PLAYLISTS_TESTS};
